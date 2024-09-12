bd7160d0623c4ab26fcb6bd4839f9b6221af8801
zaemon1251-hesty <hisakawa1251@gmail.com>
2024-09-12 12:14:14 +0900

    add exp03 (skipping pitch and calib)

--- sn_gamestate/calibration/pitch.py
+++ sn_gamestate/calibration/pitch.py
@@ -6,8 +6,11 @@ import gdown
 import numpy as np
 import pandas as pd
 
-from sn_calibration_baseline.detect_extremities import SegmentationNetwork, \
-    generate_class_synthesis, get_line_extremities
+from sn_calibration_baseline.detect_extremities import (
+    SegmentationNetwork,
+    generate_class_synthesis,
+    get_line_extremities,
+)
 from tracklab.pipeline import ImageLevelModule
 
 
@@ -15,9 +18,16 @@ class BaselinePitch(ImageLevelModule):
     input_columns = []
     output_columns = []
 
-    def __init__(self, mean_file, std_file, model_weights,
-                 resolution_width, resolution_height,
-                 batch_size, **kwargs):
+    def __init__(
+        self,
+        mean_file,
+        std_file,
+        model_weights,
+        resolution_width,
+        resolution_height,
+        batch_size,
+        **kwargs
+    ):
         super().__init__(batch_size=batch_size)
         self.download_model(model_weights)
         self.model_weights = model_weights
@@ -29,12 +39,16 @@ class BaselinePitch(ImageLevelModule):
     def download_model(self, model_weights):
         if Path(model_weights).stem == "soccer_pitch_segmentation":
             md5 = "229023fbbd7e51c1abc56825d3492874"
-            gdown.cached_download(id="1dbN7LdMV03BR1Eda8n7iKNIyYp9r07sM", path=model_weights, md5=md5)
+            gdown.cached_download(
+                id="1dbN7LdMV03BR1Eda8n7iKNIyYp9r07sM", path=model_weights, md5=md5
+            )
 
     def preprocess(self, image, detections: pd.DataFrame, metadata: pd.Series) -> Any:
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        image = cv2.resize(image, (self.model.width, self.model.height), interpolation=cv2.INTER_LINEAR)
-        image = np.asarray(image, np.float32) / 255.
+        image = cv2.resize(
+            image, (self.model.width, self.model.height), interpolation=cv2.INTER_LINEAR
+        )
+        image = np.asarray(image, np.float32) / 255.0
         image = (image - self.model.mean) / self.model.std
         image = image.transpose((2, 0, 1))
         return image
@@ -47,11 +61,16 @@ class BaselinePitch(ImageLevelModule):
         for result, idx in zip(results, metadatas.index):
             semlines = np.asarray(np.argmax(result, axis=2), dtype=np.uint8)
             skeletons = generate_class_synthesis(semlines, radius=6)
-            extremities = get_line_extremities(skeletons, maxdist=40,
-                                               width=self.resolution_width,
-                                               height=self.resolution_height)
-            output_pred.append(pd.Series(
-                {"lines": extremities},
-                name=idx,
-            ))
+            extremities = get_line_extremities(
+                skeletons,
+                maxdist=40,
+                width=self.resolution_width,
+                height=self.resolution_height,
+            )
+            output_pred.append(
+                pd.Series(
+                    {"lines": extremities},
+                    name=idx,
+                )
+            )
         return pd.DataFrame(), pd.DataFrame(output_pred)
--- sn_gamestate/team/__init__.py
+++ sn_gamestate/team/__init__.py
@@ -1,2 +1,5 @@
 from .tracklet_team_clustering_api import TrackletTeamClustering
 from .tracklet_team_side_labeling_api import TrackletTeamSideLabeling
+from .tracklet_team_side_labeling_with_naive_bbox_api import (
+    TrackletTeamSideLabelingWithNaiveBbox,
+)
